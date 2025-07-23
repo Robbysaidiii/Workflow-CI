a86f6074b7cca94b817a68646e6cae22dbfcf7b5
Robby Saidi <robbysaidiii@gmail.com>
2025-07-24 02:22:53 +0700

    fix: rename python_env.yaml to conda.yaml

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import mlflow
 import joblib
+import shutil
+from mlflow.artifacts import download_artifacts
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -11,9 +13,7 @@ from sklearn.metrics import accuracy_score
 
 
 def main(data_path):
-    # Start MLflow run
-    with mlflow.start_run():
-
+    with mlflow.start_run() as run:
         # Load dataset
         df = pd.read_csv(data_path)
 
@@ -22,38 +22,43 @@ def main(data_path):
         le = LabelEncoder()
         y = le.fit_transform(df["salary_bin"])
 
-        # Train-test split
+        # Split data
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, stratify=y, test_size=0.2, random_state=42
         )
 
-        # Model training
+        # Train model
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
 
-        # Evaluation
+        # Evaluate
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
         print(f"✅ Akurasi: {acc:.4f}")
 
-        # Logging to MLflow
+        # Log metrics
         mlflow.log_param("n_estimators", 100)
         mlflow.log_param("random_state", 42)
         mlflow.log_metric("accuracy", acc)
 
-        # Save artifacts
+        # Log raw model & label encoder
         os.makedirs("outputs", exist_ok=True)
-        model_path = "outputs/model.pkl"
-        le_path = "outputs/label_encoder.pkl"
+        joblib.dump(model, "outputs/model.pkl")
+        joblib.dump(le, "outputs/label_encoder.pkl")
+        mlflow.log_artifact("outputs/model.pkl")
+        mlflow.log_artifact("outputs/label_encoder.pkl")
 
-        joblib.dump(model, model_path)
-        joblib.dump(le, le_path)
-
-        # Log artifacts to MLflow
-        mlflow.log_artifact(model_path)
-        mlflow.log_artifact(le_path)
-
+        # Log as MLflow model
         mlflow.sklearn.log_model(model, artifact_path="sklearn-model")
+
+        # Download model artifact from remote (DAGsHub)
+        run_id = run.info.run_id
+        source_path = download_artifacts(artifact_uri=f"runs:/{run_id}/sklearn-model")
+        target_path = "outputs/mlflow-model"
+
+        if os.path.exists(target_path):
+            shutil.rmtree(target_path)
+        shutil.copytree(source_path, target_path)
 
 
 if __name__ == "__main__":
